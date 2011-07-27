e55361260f90c351d329d32579c48226fa74fa87
James Jones <jamjon3@gmail.com>
2011-07-27 09:21:33 -0400

    Doing some preliminary stuff for JQuery mobile support

--- Voter/Main.pm
+++ Voter/Main.pm
@@ -1,14 +1,14 @@
 package Voter::Main;
 @ISA = qw(Voter);
 
-use lib qw(.); 
+use lib qw(.);
 use strict;
 
 sub new {
     my($type) = $_[0];
     my($self) = {};
     bless($self,$type);
-    return($self);    
+    return($self);
 }
 
 sub showMain {
@@ -16,7 +16,7 @@ sub showMain {
     my($r) = $_[1];
     $r->content_type('text/html');
     my($html) = qq{
-    
+
         <HTML>
             <HEAD>
                 <TITLE>Hello World</TITLE>
@@ -26,11 +26,11 @@ sub showMain {
                 <script type="text/javascript">
                     // has the google object loaded?
                     if (window.google && window.google.load) {
-                        google.load("jquery", "1.6.1");
+                        google.load("jquery", "1.6.2");
                         google.load("jqueryui", "1.8.13");
                         google.setOnLoadCallback(function() {
                             \$('div#content').voter({
-                                
+
                                 });
                             //var audioFormTextbox = $('<textarea />')
                             //    .appendTo('div#inlineExample')
@@ -41,22 +41,28 @@ sub showMain {
                             //    .click(function() {
                             //        audioFormTextbox.textToAudio('play');
                             //    });
-            
+
                         });
                     }
                 </script>
+                <link rel="stylesheet" href="http://code.jquery.com/mobile/1.0b1/jquery.mobile-1.0b1.min.css" />
+                <script src="http://code.jquery.com/mobile/1.0b1/jquery.mobile-1.0b1.min.js"></script>
                 <script type='text/javascript' src='lib/jquery.ui.voter.js'></script>
             </HEAD>
             <BODY>
-                <H4>Hello World</H4>
-                <P>
-                    Your IP Address is $ENV{REMOTE_ADDR} and Home is $ENV{HOME}
-                <P>
-                <div id='content' ></div>
-                <H5>Have a "nice" day</H5>
-                <p>Name:<input type="text" size="20" name="txt_name" value="name">
+                <div data-role="page">
+                    <div data-role="content">
+                        <H4>Hello World</H4>
+                        <P>
+                            Your IP Address is $ENV{REMOTE_ADDR}
+                        <P>
+                        <div id='content' ></div>
+                        <H5>Have a "nice" day</H5>
+                        <p>Name:<input type="text" size="20" name="txt_name" value="name">
+                    </div>
+                </div>
             </BODY>
         </HTML>
-    };    
-    print $html;    
+    };
+    print $html;
 }
